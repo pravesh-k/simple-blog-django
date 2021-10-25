dd3e1936e476a8384226302d29b4cea16fa96634
Pravesh <praveshnayak@gmail.com>
2021-10-26 00:59:08 +0530

    feature: form for comment model

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:                 # indicate which model to use and django will automatically
+        model = Comment         # introspect and builds the from dynamically
+        fields = ('name', 'email', 'body')      # only this 3 fields need to appear in the
+                                                # form as user can fill on these 3
