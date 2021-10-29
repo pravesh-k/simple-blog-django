303afe6623c6bcddc7fd289caa41837bcbdf9ea6
Pravesh <praveshnayak@gmail.com>
2021-10-30 01:03:40 +0530

    feature: querying similar posts based on tags

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 # view for post list
@@ -69,11 +70,16 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.value_list('id', flat=True)
+    similar_posts = Post.publish.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
     args = {                                # data to be passed to the template for rendering
             'post': post,
             'comments': comments,
             'new_comment': new_comment,
-            'comment_form': comment_form
+            'comment_form': comment_form,
+            'similar_posts': similar_posts
             }
             
     return render(
