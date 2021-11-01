386cadd1f295b563a4f93c4ae81ecb36d8feba0f
Pravesh <praveshnayak@gmail.com>
2021-11-01 16:08:40 +0530

    feature: render most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,16 +1,24 @@
 from django import template
 from django.db import reset_queries
 from ..models import Post
+from django.db.models import Count
 
+# to register custom template tags and filters
 register = template.Library()
 
+# template tag to return the total number of published posts
 @register.simple_tag
 def total_posts():
     return Post.published.count()
 
+# template tag to return the latest published posts
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    # print('\n\n\n', latest_posts, '\n\n\n')
     args = {'latest_posts': latest_posts}
     return args
+
+# template tag to return the posts with max comments
+@register.simple_tag
+def get_most_commented_posts(count = 5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
