de3b72588789a521f39fe4cc3601b57d220636f6
Pravesh <praveshnayak@gmail.com>
2021-11-01 19:18:01 +0530

    feature: template filter to enable markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,6 +2,8 @@ from django import template
 from django.db import reset_queries
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 # to register custom template tags and filters
 register = template.Library()
@@ -22,3 +24,8 @@ def show_latest_posts(count=5):
 @register.simple_tag
 def get_most_commented_posts(count = 5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+# template filter to enable us to use markdown
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
