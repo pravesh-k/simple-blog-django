8e57b5e559b5322d2eefdf779addbc8ec6abfd91
Pravesh <praveshnayak@gmail.com>
2021-11-01 14:40:05 +0530

    feature: create inclusion custom tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db import reset_queries
 from ..models import Post
 
 register = template.Library()
@@ -6,3 +7,10 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    # print('\n\n\n', latest_posts, '\n\n\n')
+    args = {'latest_posts': latest_posts}
+    return args
