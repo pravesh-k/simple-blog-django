40de534d8b26f69ff4df8faa9ebe6a303316cec8
Pravesh <praveshnayak@gmail.com>
2021-11-03 23:40:49 +0530

    feature: class form for Search function

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,21 @@
 from django import forms
+from django.db.models import query
 from .models import Comment
 
-
+# Form for post sharing functionality
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
-
+# Form for comments in a post
 class CommentForm(forms.ModelForm):
     class Meta:                 # indicate which model to use and django will automatically
-        model = Comment         # introspect and builds the from dynamically
-        fields = ('name', 'email', 'body')      # only this 3 fields need to appear in the
+        model = Comment         # introspect and builds the form dynamically
+        fields = ('name', 'email', 'body')      # only these 3 fields need to appear in the
                                                 # form as user can fill on these 3
+
+# Form for full text search functionality
+class SearchForm(forms.Form):
+    query = forms.CharField()
